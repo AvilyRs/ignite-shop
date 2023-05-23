ca1886ffa5923313ebc9b9f9d178a13656bcfbfa
Avily Silva <avilyrs@gmail.com>
2023-05-23 14:39:26 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ignite Shop</title>
+        <meta name="description" content="An ignite shop application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Main content</h1>
+      </main>
+    </>
+  )
+}
