b82646e02701855855dfbd08e5948c1acbc103ce
Avily Silva <avilyrs@gmail.com>
2023-05-25 11:24:17 -0300

    :feat: keen slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+
 import tshirt1 from 'src/assets/tshirts/1.png';
 import tshirt2 from 'src/assets/tshirts/2.png';
-// import tshirt3 from 'src/assets/tshirts/3.png';
-// import tshirt4 from 'src/assets/tshirts/4.png';
+import tshirt3 from 'src/assets/tshirts/3.png';
+import tshirt4 from 'src/assets/tshirts/4.png';
 
 import { HomeContainer } from 'src/styles/pages/home';
 import { Product } from 'src/styles/pages/home';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
     <>
       <Head>
@@ -17,8 +27,8 @@ export default function Home() {
         <meta name="description" content="An ignite shop application" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <HomeContainer>
-        <Product href="#">
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        <Product href="#" className="keen-slider__slide">
           <Image src={tshirt1} width={520} height={480} alt="" />
 
           <footer>
@@ -26,8 +36,25 @@ export default function Home() {
             <span>R$ 79,90</span>
           </footer>
         </Product>
-        <Product href="#">
+        <Product href="#" className="keen-slider__slide">
           <Image src={tshirt2} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product href="#" className="keen-slider__slide">
+          <Image src={tshirt3} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+        <Product href="#" className="keen-slider__slide">
+          <Image src={tshirt4} width={520} height={480} alt="" />
 
           <footer>
             <strong>Camiseta</strong>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from '..';
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
 
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
@@ -17,7 +16,6 @@ export const Product = styled('a', {
 
   borderRadius: 8,
   cursor: 'pointer',
-  padding: '0.25rem',
   position: 'relative',
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
 
